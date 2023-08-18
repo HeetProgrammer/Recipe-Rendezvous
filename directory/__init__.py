7e038e42137546092bfb10a3e8e6a7b552f551bd
Heet <aofavalon@gmail.com>
2023-08-18 21:09:29 +0530

    Finished Project

--- directory/__init__.py
+++ directory/__init__.py
@@ -14,6 +14,10 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_page'
 login_manager.login_message_category = 'info'
+with open('directory/api_key.txt') as f:
+    api_key = f.read()
+
+
 
 
 
--- directory/routes.py
+++ directory/routes.py
@@ -1,13 +1,42 @@
-from directory import app, db
+from directory import app, db, api_key
 from flask import render_template, redirect, url_for, flash, request
 from directory.models import User
 from directory.forms import RegisterForm, LoginForm
 from flask_login import login_user, logout_user, login_required, current_user
+import requests
+
+headers = {
+    "x-api-key": api_key,
+}
+random_recipes_url = "https://api.spoonacular.com/recipes/random"
+
+
 
 @app.route("/")
-@app.route("/home")
-def home_page():
-    return render_template("index.html")
+@app.route("/home<tags>")
+def home_page(tags=None):
+    results = []
+
+    if current_user.is_authenticated:
+        if not tags:
+        
+            params = {
+                "number": 10,
+            }
+            response = requests.get(random_recipes_url, headers=headers, params=params)
+            results = response.json()
+            print(results)
+        else:
+            params = {
+            "number": 10,
+            "tags": tags
+            }
+            response = requests.get(random_recipes_url, headers=headers, params=params)
+            results = response.json()
+            print(results)
+
+
+    return render_template("index.html", results=results)
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -38,10 +67,45 @@ def login_page():
                 login_user(attempted_user)
                 flash(f"Success! You are logged in as {attempted_user.username} ", category="success")
                 return redirect(url_for('home_page'))
+    
+    for error in form.errors:
+        if error:
+            flash(error, category='danger')
     return render_template('login.html', form=form)
 
+@login_required
 @app.route('/logout')
 def logout_page():
     logout_user()
     flash("You have been logged out!", category="info")
     return redirect(url_for('home_page'))
+
+@login_required
+@app.route("/filter", methods=["GET", "POST"])
+def filter_results():
+    if request.method == "POST":
+        options = ['vegetarian', 'non-vegetarian', 'indian', 'japanese', 'american', 'italian', 'mexican', 'spanish', 'french', 'chinese']
+        checked_options = []
+        print(request.form)
+        for option in options:
+            if option in request.form:
+                checked_options.append(option)
+        
+        tags = ','.join(checked_options)
+        print(checked_options)
+        print(tags)
+        return redirect(f'/home{tags}')
+
+
+@login_required
+@app.route("/recipe_info/<int:id>")
+def recipe_info(id):
+    recipe_info_url = f"https://api.spoonacular.com/recipes/{id}/information"
+    response = requests.get(recipe_info_url, headers=headers)
+    result = response.json()
+    return render_template("recipe_info.html", result=result)
+
+        
+        
+
+
